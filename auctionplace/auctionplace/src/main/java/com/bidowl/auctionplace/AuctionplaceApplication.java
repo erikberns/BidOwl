5abad5990706f989fcb5903fe623b1dba87c90d5
Agustin Blanco Vocos <ablancovocos@uade.edu.ar>
2026-05-11 14:38:43 -0300

    Spring Initializer
    Archivos del SprinBoot

---
+++ auctionplace/auctionplace/src/main/java/com/bidowl/auctionplace/AuctionplaceApplication.java
@@ -0,0 +1,13 @@
+package com.bidowl.auctionplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionplaceApplication.class, args);
+	}
+
+}
---
+++ auctionplace/auctionplace/src/test/java/com/bidowl/auctionplace/AuctionplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidowl.auctionplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
